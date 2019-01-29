02226f796a129dd0e4d7d80b7c9258c2e93ae802
Nathan Olsen <Nsolsen@coe.edu>
2019-01-29 09:35:02 -0600

    Create sizeof.c

---
+++ sizeof.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+printf("The size of int is %i \n",sizeof(int));
+printf("The size of char is %i \n",sizeof(char));
+printf("The size of long is %i \n",sizeof(long));
+printf("The size of float is %i \n",sizeof(float));
+printf("The size of double is %i \n",sizeof(double));
+printf("The size of unsigned int is %i \n",sizeof(unsigned int));
+printf("The size of unsigned char is %i \n",sizeof(unsigned char));
+printf("The size of unsigned long is %i \n",sizeof(unsigned long));
+printf("The size of int * is %i \n",sizeof(int*));
+printf("The size of char * is %i \n",sizeof(char*));
+printf("The size of long * is %i \n",sizeof(long*));
+}
